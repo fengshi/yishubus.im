8695984c9aedaca2720810146ede257d80c30f82
shifeng <shifeng007@gmail.com>
2013-05-23 11:56:23 +0800

    no message

---
+++ ArtIMessage/ArtIMessage/ArtIMessageAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ArtIMessageAppDelegate.h
+//  ArtIMessage
+//
+//  Created by shi feng on 13-5-23.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ArtIMessageViewController;
+
+@interface ArtIMessageAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ArtIMessageViewController *viewController;
+
+@end
